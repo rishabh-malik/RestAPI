ed6d2c6c8412954556a698eb25df3e8d133d8f75
rishabh-malik <rishabhmalik249@gmail.com>
2018-12-10 19:58:42 +0530

    update done

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,11 @@ router.post('/ninjas',function(req,res,next){
 
 //update ninja in db
 router.put('/ninjas/:id',function(req,res,next){
-    res.send({type:'PUT'});
+    Ninja.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
+        Ninja.findOne({_id:req.params.id}).then(function(ninja){
+            res.send(ninja);
+        });
+    }).catch(next);
 });
 
 // delete a ninja from the db
